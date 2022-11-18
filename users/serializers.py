9c169dfbd28e6139c66fb65f5383201d4e47f82a
NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
2022-11-18 16:21:11 +0600

    User models commit

---
+++ users/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import UserProfile, CustomUser
+from phone_field import PhoneField
+
+
+class UsersProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField()
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'username', 'password', 'password2', 'phone']
+
+    def save(self, *args, **kwargs):
+        # Создаём объект класса User
+        user = CustomUser(
+            email=self.validated_data['email'],
+            username=self.validated_data['username'],
+            phone=self.validated_data['phone'],
+
+        )
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        if password != password2:
+            raise serializers.ValidationError({password: "Пароль не совпадает"})
+        user.set_password(password)
+        user.save()
+        return user
---
+++ users/views.py
@@ -0,0 +1,48 @@
+from urllib import request
+
+from rest_framework.generics import (ListCreateAPIView, RetrieveUpdateDestroyAPIView, )
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import viewsets, generics, status
+from rest_framework.response import Response
+
+from .models import CustomUser
+from .permissions import IsOwnerProfileOrReadOnly
+from .serializers import UsersProfileSerializer, UserRegisterSerializer
+
+
+class RegisterUserView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny, ]
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserRegisterSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data['response'] = True
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            data = serializer.errors
+            return Response(data)
+
+
+class UserProfileListCreateView(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UsersProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(user=user)
+
+
+class UserProfileDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UsersProfileSerializer
+    permission_classes = [IsOwnerProfileOrReadOnly, IsAuthenticated]
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+    serializer_class = UsersProfileSerializer
+    queryset = CustomUser.objects.all()
