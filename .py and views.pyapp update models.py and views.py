64b6bb1ce75636cbfa33b9b23e26a294d55feac2
NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
2022-11-25 11:01:05 +0600

    test

---
+++ .py and views.pyapp update models.py and views.py
@@ -0,0 +1,87 @@
+[33mcommit dde61cac15565f6907a9bd70e8a67a7e6bb1fc17[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Reflog: HEAD@{0} (NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>)
+Reflog message: merge origin/master: Fast-forward
+Author: Nurzhigit <110616648+NurzhigitAtashbaev@users.noreply.github.com>
+Date:   Wed Nov 23 20:42:31 2022 +0600
+
+    Rename readme.md to README.md
+
+[33mcommit 56c5abbc028b181a0618303451e412656f926ef4[m
+Reflog: HEAD@{1} (NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>)
+Reflog message: checkout: moving from staf-info to master
+Author: NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
+Date:   Wed Nov 23 12:24:42 2022 +0600
+
+    new app commit
+
+[33mcommit 56c5abbc028b181a0618303451e412656f926ef4[m
+Reflog: HEAD@{2} (NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>)
+Reflog message: reset: moving to HEAD~
+Author: NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
+Date:   Wed Nov 23 12:24:42 2022 +0600
+
+    new app commit
+
+[33mcommit 761b1474ceaa51760c92880d486e08ce31e759cb[m
+Reflog: HEAD@{3} (NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>)
+Reflog message: reset: moving to HEAD
+Author: NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
+Date:   Fri Nov 25 00:48:49 2022 +0600
+
+    app update models.py and views.py
+
+[33mcommit 761b1474ceaa51760c92880d486e08ce31e759cb[m
+Reflog: HEAD@{4} (NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>)
+Reflog message: commit: app update models.py and views.py
+Author: NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
+Date:   Fri Nov 25 00:48:49 2022 +0600
+
+    app update models.py and views.py
+
+[33mcommit 56c5abbc028b181a0618303451e412656f926ef4[m
+Reflog: HEAD@{5} (NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>)
+Reflog message: checkout: moving from master to staf-info
+Author: NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
+Date:   Wed Nov 23 12:24:42 2022 +0600
+
+    new app commit
+
+[33mcommit 56c5abbc028b181a0618303451e412656f926ef4[m
+Reflog: HEAD@{6} (NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>)
+Reflog message: commit: new app commit
+Author: NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
+Date:   Wed Nov 23 12:24:42 2022 +0600
+
+    new app commit
+
+[33mcommit 448730d14fafc3b0bc4c477926bd826ecd342074[m
+Reflog: HEAD@{7} (NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>)
+Reflog message: commit: superuser registration line add phone number
+Author: NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
+Date:   Mon Nov 21 17:25:35 2022 +0600
+
+    superuser registration line add phone number
+
+[33mcommit a9bb9a6d9844bbc3c258557911645dbcf56ca988[m
+Reflog: HEAD@{8} (NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>)
+Reflog message: commit: send email commit
+Author: NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
+Date:   Sat Nov 19 19:25:40 2022 +0600
+
+    send email commit
+
+[33mcommit 9c169dfbd28e6139c66fb65f5383201d4e47f82a[m
+Reflog: HEAD@{9} (NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>)
+Reflog message: commit: User models commit
+Author: NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
+Date:   Fri Nov 18 16:21:11 2022 +0600
+
+    User models commit
+
+[33mcommit 50558bda72efeaf9540482a8e53ffabd2b51a2ce[m
+Reflog: HEAD@{10} (NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>)
+Reflog message: commit (initial): README.md file commit
+Author: NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
+Date:   Mon Nov 14 23:47:57 2022 +0600
+
+    README.md file commit
