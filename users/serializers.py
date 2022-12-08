e517f7bdcd63e58c43a70be1b4a5872bc629b79a
NurzhigitAtashbaev <atashbaevnurjigit@gmail.com>
2022-12-09 02:48:24 +0600

    UserRegisterSerializer update"

--- users/serializers.py
+++ users/serializers.py
@@ -10,8 +10,30 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ('email', 'username', 'password', 'password2', 'phone')
 
+    def validate_email(self, email):
+        # Check that the email address is unique
+        if CustomUser.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Email address already in use')
+        return email
+
+    def validate_username(self, username):
+        # Check that the username is unique
+        if CustomUser.objects.filter(username=username).exists():
+            raise serializers.ValidationError('Username already exists')
+        return username
+
+    def validate_password(self, password):
+        # Check that the password meets certain requirements
+        if len(password) < 8:
+            raise serializers.ValidationError('Password is too short (minimum is 8 characters)')
+        if not any(char.isdigit() for char in password):
+            raise serializers.ValidationError('Password must contain at least one digit')
+        if not any(char.isalpha() for char in password):
+            raise serializers.ValidationError('Password must contain at least one letter')
+        return password
+
     def save(self, *args, **kwargs):
-        # Создаём объект класса CustomUser
+        # Create a CustomUser object
         user = CustomUser(
             email=self.validated_data['email'],
             username=self.validated_data['username'],
@@ -20,11 +42,10 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         password = self.validated_data['password']
         password2 = self.validated_data['password2']
         if password != password2:
-            raise serializers.ValidationError({password: "Пароль не совпадает"})
+            raise serializers.ValidationError({password: "Passwords do not match"})
         user.set_password(password)
         user.save()
         return user
-
 
 class UsersProfileSerializer(serializers.ModelSerializer):
     class Meta:
