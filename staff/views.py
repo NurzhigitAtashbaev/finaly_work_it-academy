b371e81e3f8b296f256aa79f23b69edb3d95b53a
Aimerim <aimeerimt68@gmail.com>
2022-12-08 14:35:35 +0600

    view,serializer для контактов

--- staff/views.py
+++ staff/views.py
@@ -1,8 +1,13 @@
 from rest_framework import generics
-
 from .models import AboutStaff, Contacts
-from .serializers import StaffListSerializer
+from .serializers import StaffListSerializer, ContactSerializer
+
 
 class StaffListAPIView(generics.ListAPIView):
     queryset = AboutStaff.objects.all()
     serializer_class = StaffListSerializer
+
+
+class ContactsListAPIView(generics.ListAPIView):
+    queryset = Contacts.objects.all()
+    serializer_class = ContactSerializer
